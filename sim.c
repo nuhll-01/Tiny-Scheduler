d2c7b384edd5c5882e7c6448818ea07680838fce
ToastyyX <erick.cool321@icloud.com>
2025-02-18 13:32:08 -0600

    adding some sort of skeleton

--- sim.c
+++ sim.c
@@ -1,5 +1,22 @@
 #include "utils.h"
 
-void run_rr(int argc, char *argv[]) {  }
+void run_rr(int argc, char *argv[]) { 
+    puts("\nRunning RR...");
+    puts("Time | Running | Ready Queue");
+    puts("-----------------------------");
 
-void run_fcfs(int argc, char *argv[]) {  }
+    // some logic here
+
+    puts("");
+
+
+    puts("\nRR Execution Completed.");
+    puts("Average Response Time:");
+    puts("Average Turnaround Time:");
+    puts("Total Context Switches:");
+
+
+    puts("");
+}
+
+void run_fcfs(int argc, char *argv[]) { printf("%d\n", argc); }
